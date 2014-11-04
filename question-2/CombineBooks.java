3d989e594af60e17f5185f7e8d2489d02e7af17d
Asgeir Bjarnason <asgbja@gmail.com>
2014-11-04 13:39:26 +0200

    question-2-a fixed..

--- question-2/CombineBooks.java
+++ question-2/CombineBooks.java
@@ -1,126 +1,104 @@
 package org.hwone;
-//package my.books;
-import java.io.IOException;
+
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.IOException;
 import org.json.*;
 
-import javax.security.auth.callback.TextInputCallback;
+/*
+*  Modify this file to combine books from the same other into
+*  single JSON object. 
+*  i.e. {"author": "Tobias Wells", "books": [{"book":"A die in the country"},{"book": "Dinky died"}]}
+*  Beaware that, this may work on anynumber of nodes! 
+*
+*/
 
 public class CombineBooks {
 
-    public static class Map extends Mapper<LongWritable, Text, Text, Text>{
+	public static class Map extends Mapper<LongWritable, Text, Text, Text>{
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
 
-        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+			String author;
+			String book;
+			String line = value.toString();
+			String[] authorBookTuple = line.split("\\n");
 
-            String author;
-            String book;
-            String line = value.toString();
-            String[] tuple = line.split("\\n");
-            try{
-                for(int i=0;i<tuple.length; i++){
-                    JSONObject obj = new JSONObject(tuple[i]);
-                    author = obj.getString("author");
-                    book = obj.getString("book");
-                    context.write(new Text(author), new Text(book));
-                }
-            }catch(JSONException e){
-                e.printStackTrace();
-            }
-        }
-    }
+			try{
+				for(int i = 0; i < authorBookTuple.length; i++){
 
-    public static class Reduce extends Reducer<Text,Text,NullWritable,Text>{
+					JSONObject obj = new JSONObject(authorBookTuple[i]);
+					author = obj.getString("author");
+					book = obj.getString("book");
+					context.write(new Text(author), new Text(book));
 
-        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
+				}
+			}
+			catch(JSONException e){
+				e.printStackTrace();
+			}
+		}
+	}
 
-            try{
-                JSONObject obj = new JSONObject();
-                JSONArray ja = new JSONArray();
-                for(Text val : values){
-                    JSONObject jo = new JSONObject().put("book", val.toString());
-                    ja.put(jo);
-                }
-                obj.put("books", ja);
-                obj.put("author", key.toString());
-                context.write(NullWritable.get(), new Text(obj.toString()));
-            }catch(JSONException e){
-                e.printStackTrace();
-            }
-        }
-    }
+	public static class Reduce extends Reducer<Text,Text,NullWritable,Text>{
 
-    public static void main(String[] args) throws Exception {
-        Configuration conf = new Configuration();
-        if (args.length != 2) {
-            System.err.println("Usage: CombineBooks <in> <out>");
-            System.exit(2);
-        }
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
 
-        Job job = new Job(conf, "CombineBooks");
-        job.setJarByClass(CombineBooks.class);
-        job.setMapperClass(Map.class);
-        job.setReducerClass(Reduce.class);
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(Text.class);
-        job.setOutputKeyClass(NullWritable.class);
-        job.setOutputValueClass(Text.class);
-        job.setInputFormatClass(TextInputFormat.class);
-        job.setOutputFormatClass(TextOutputFormat.class);
+			try{
 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+				JSONObject obj = new JSONObject();
+				JSONArray array = new JSONArray();
+				for(Text val : values){
 
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
-    }
+					JSONObject tempObj = new JSONObject().put("book", val.toString());
+					array.put(tempObj);
+					
+				}
+				obj.put("books", array);
+				obj.put("author", key.toString());
+				context.write(NullWritable.get(), new Text(obj.toString()));
+
+			}
+			catch(JSONException e){
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args)
+		.getRemainingArgs();
+		if (otherArgs.length != 2) {
+		  System.err.println("Usage: CombineBooks <in> <out>");
+		  System.exit(2);
+	  }
+
+	  Job job = new Job(conf, "CombineBooks");
+	  job.setJarByClass(CombineBooks.class);
+	  job.setMapperClass(Map.class);
+	  job.setReducerClass(Reduce.class);
+	  job.setMapOutputKeyClass(Text.class);
+	  job.setMapOutputValueClass(Text.class);
+	  job.setOutputKeyClass(NullWritable.class);
+	  job.setOutputValueClass(Text.class);
+	  job.setInputFormatClass(TextInputFormat.class);
+	  job.setOutputFormatClass(TextOutputFormat.class);
+
+	  FileInputFormat.addInputPath(job, new Path(args[0]));
+	  FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+	  System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
 }
-
-// package org.hwone;
-
-// import org.apache.hadoop.conf.Configuration;
-// import org.apache.hadoop.mapreduce.Job;
-// import org.apache.hadoop.util.GenericOptionsParser;
-
-// //TODO import necessary components
-
-// /*
-// *  Modify this file to combine books from the same other into
-// *  single JSON object. 
-// *  i.e. {"author": "Tobias Wells", "books": [{"book":"A die in the country"},{"book": "Dinky died"}]}
-// *  Beaware that, this may work on anynumber of nodes! 
-// *
-// */
-
-// public class CombineBooks {
-
-//   //TODO define variables and implement necessary components
-
-//   public static void main(String[] args) throws Exception {
-//     Configuration conf = new Configuration();
-//     String[] otherArgs = new GenericOptionsParser(conf, args)
-// 				.getRemainingArgs();
-//     if (otherArgs.length != 2) {
-//       System.err.println("Usage: CombineBooks <in> <out>");
-//       System.exit(2);
-//     }
-
-//     //TODO implement CombineBooks
-
-//     Job job = new Job(conf, "CombineBooks");
-
-//     //TODO implement CombineBooks
-
-//     System.exit(job.waitForCompletion(true) ? 0 : 1);
-//   }
-// }
