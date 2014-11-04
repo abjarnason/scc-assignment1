fb1ef96ace401f8ff574123bb60ddbdaf8a14872
Asgeir Bjarnason <asgbja@gmail.com>
2014-11-04 14:36:25 +0200

    question-2-b first commit

--- question-2/QueryAuthor.java
+++ question-2/QueryAuthor.java
@@ -3,8 +3,19 @@ package org.hwone;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-//TODO import necessary components
+import java.io.IOException;
+import org.json.*;
 
 /*
 *  Modify this file to return single combined books from the author which
@@ -17,23 +28,86 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class QueryAuthor {
 
-  //TODO define variables and implement necessary components
+	public static class Map extends Mapper<LongWritable, Text, Text, Text>{
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
 
-  public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    String[] otherArgs = new GenericOptionsParser(conf, args)
+			public void configure(JobConf jc){
+				authorQuery = jc.get("authorQuery");
+			}
+
+			String author;
+			String book;
+			String line = value.toString();
+			String[] authorBookTuple = line.split("\\n");
+
+			try{
+				for(int i = 0; i < authorBookTuple.length; i++){
+
+					if(word.equalsIgnoreCase(authorQuery)){
+
+						JSONObject obj = new JSONObject(authorBookTuple[i]);
+						author = obj.getString("author");
+						book = obj.getString("book");
+						context.write(new Text(author), new Text(book));
+					
+					}
+				}
+			}
+			catch(JSONException e){
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static class Reduce extends Reducer<Text,Text,NullWritable,Text>{
+
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
+
+			try{
+
+				JSONObject obj = new JSONObject();
+				JSONArray array = new JSONArray();
+				for(Text val : values){
+
+					JSONObject tempObj = new JSONObject().put("book", val.toString());
+					array.put(tempObj);
+					
+				}
+				obj.put("books", array);
+				obj.put("author", key.toString());
+				context.write(NullWritable.get(), new Text(obj.toString()));
+
+			}
+			catch(JSONException e){
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args)
 				.getRemainingArgs();
-    if (otherArgs.length < 3) {
-      System.err.println("Usage: QueryAuthor <in> <out> <author>");
-      System.exit(2);
-    }
+		if (otherArgs.length < 3) {
+			System.err.println("Usage: QueryAuthor <in> <out> <author>");
+			System.exit(2);
+		}
 
-    //TODO implement QueryAuthor
+		Job job = new Job(conf, "QueryAuthor");
+	  job.setJarByClass(CombineBooks.class);
+	  job.setMapperClass(Map.class);
+	  job.setReducerClass(Reduce.class);
+	  job.setMapOutputKeyClass(Text.class);
+	  job.setMapOutputValueClass(Text.class);
+	  job.setOutputKeyClass(NullWritable.class);
+	  job.setOutputValueClass(Text.class);
+	  job.setInputFormatClass(TextInputFormat.class);
+	  job.setOutputFormatClass(TextOutputFormat.class);
 
-    Job job = new Job(conf, "QueryAuthor");
+	  FileInputFormat.addInputPath(job, new Path(args[0]));
+	  FileOutputFormat.setOutputPath(job, new Path(args[1])); 
+	  conf.set("authorQuery", args[2]);
 
-    //TODO implement QueryAuthor
-
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
 }
