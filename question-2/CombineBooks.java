7cae811c6fb4d0a32f17463eb6dc320a8b1041d2
Asgeir Bjarnason <asgbja@gmail.com>
2014-11-10 11:13:52 +0200

    question 2 a updated

--- question-2/CombineBooks.java
+++ question-2/CombineBooks.java
@@ -16,6 +16,7 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 import org.json.*;
+import java.util.StringTokenizer
 
 /*
 *  Modify this file to combine books from the same other into
@@ -51,20 +52,46 @@ public class CombineBooks {
 		}
 	}
 
+	public static class Combine extends Reducer<Text, Text, Text, Text>{
+
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
+
+			String books = null;
+			for(Text val : values){
+				if(books != null){
+					books = booklist + "," + val.toString();
+				}
+				else{
+					books = val.toString();
+				}
+			}
+			context.write(key, new Text(books));
+		}
+  }
+
 	public static class Reduce extends Reducer<Text,Text,NullWritable,Text>{
 
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
 
 			try{
 
-				JSONObject obj = new JSONObject();
+				
 				JSONArray array = new JSONArray();
-				for(Text val : values){
+				String[] book = null;
 
+				for(Text val : values){
+					book = val.toString().split(",");
+					/*
 					JSONObject tempObj = new JSONObject().put("book", val.toString());
 					array.put(tempObj);
-					
+					JSONObject obj = new JSONObject();
+					*/
 				}
+				for(int i = 0; i < book.length; i++){
+					JSONObject jsonObj = new JSONObject().put("book", book[i]);
+					array.put(jsonObj);
+				}
+				JSONObject obj = new JSONObject();
 				obj.put("books", array);
 				obj.put("author", key.toString());
 				context.write(NullWritable.get(), new Text(obj.toString()));
@@ -89,6 +116,7 @@ public class CombineBooks {
 	  job.setJarByClass(CombineBooks.class);
 	  job.setMapperClass(Map.class);
 	  job.setReducerClass(Reduce.class);
+	  job.setCombinerClass(Combine.class);
 	  job.setMapOutputKeyClass(Text.class);
 	  job.setMapOutputValueClass(Text.class);
 	  job.setOutputKeyClass(NullWritable.class);
